69da6ac7312d8ad6231823444605da589bbd2a43
Abd-Elwahhab <abdelwahhab.alshehri@gmail.com>
2021-12-18 20:14:28 +0300

    commit-003

--- js/main.js
+++ js/main.js
@@ -32,14 +32,14 @@ reFresh.onclick = function () {
 };
 
 
-//  service worker: 
+//  service worker:
 
-if ("seviceWorker" in navigator) {
-    navigator.serviceWorker.register(sw.js).then(registration => {
-        console.log("SW Registered!");
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW registered!");
         console.log(registration);
     }).catch(error => {
-        console.log("SW registration faild!");
+        console.log("SW Registration Failed");
         console.log(error);
     });
-};
+}
--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
+// self.addEventListener("install", e => {
+//     e.waitUntil(
+//         caches.open("static").then(cache => {
+//             return cache.addAll(["./", "./css/main.css", "./css/all.css", "./css/media-query.css", "./css/nova-5t-media.css", "./images/logo.svg", "./images/sdaia.svg", "./js/main.js"]);
+//         })
+//     );
+// });
+
+// self.addEventListener("fetch", e => {
+//     e.respondWith(
+//         caches.match(e.request).then(response => {
+//             return response || fetch(e.request);
+//         })
+//     );
+// });
+
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./css/main.css", "./css/all.css", "./css/media-query.css", "./css/nova-5t-media.css", "./images/logo.svg", "./images/sdaia.svg", "./js/main.js"]);
+            return cache.addAll(["./",
+                "./css/main.css",
+                "./css/all.css",
+                "./css/media-query.css",
+                "./css/nova-5t-media.css",
+                "./js/main.js",
+                "./manifest.json",
+                "./sw.js",
+                "./images/192x192.png",
+                "./images/512x512.png",
+                "./webfonts"]);
         })
     );
 });
+
 
 self.addEventListener("fetch", e => {
     e.respondWith(
